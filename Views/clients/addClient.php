191a6d60a442f55ba9226cae3d12add00fac445c
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-27 22:38:09 +0100

    add insert new client by method poo

--- Views/clients/addClient.php
+++ Views/clients/addClient.php
@@ -9,13 +9,27 @@
                 <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="name" name="name" placeholder="Enter name client">
             </div>
             <div class="flex flex-col w-2/4">
-                <label class="ml-2" for="address">Address Client <span class="text-red-600">*</span></label>
-                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="address" name="address" placeholder="Enter address client">
+                <label class="ml-2" for="email">Email <span class="text-red-600">*</span></label>
+                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="email" name="email" placeholder="Enter email client">
             </div>
         </div>
+        <div class="flex gap-3 justify-between mb-4">
+            <div class="flex flex-col w-2/4">
+                <label class="ml-2" for="address">Address Client<span class="text-red-600">*</span></label>
+                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="address" name="address" placeholder="Enter address client">
+            </div>
+            <div class="flex flex-col w-2/4">
+                <label class="ml-2" for="number_phone">Number Phone <span class="text-red-600">*</span></label>
+                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="number_phone" name="number_phone" placeholder="Enter N° phone client">
+            </div>
+        </div>
+        <div class="flex flex-col mb-4">
+            <label class="ml-2" for="password">Password <span class="text-red-600">*</span></label>
+            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="password" id="password" name="password" placeholder="password client">
+        </div>
         <div class="flex flex-col">
-            <label class="ml-2" for="phone">Number Phone <span class="text-red-600">*</span></label>
-            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" id="phone" name="phone" placeholder="Enter phone client">
+            <label class="ml-2" for="confirm_password">Confirm password <span class="text-red-600">*</span></label>
+            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="password" id="confrim_password" name="confrim_password" placeholder="confrim password client">
         </div>
         <div class="mt-5 flex justify-between">
             <button id="close" class="px-3 py-2 bg-red-600 text-white rounded-md hover:bg-red-400">Close</button>
--- Views/clients/insertClient.php
+++ Views/clients/insertClient.php
@@ -1,16 +1,22 @@
 <?php
-     require_once('../../isOwner/isOwner.php');
-    require_once('../../connectdb/connectiondb.php');
-    // insert new client
-    $name = isset($_POST['name']) ? $_POST['name'] : "";
-    $address = isset($_POST['address']) ? $_POST['address'] : "";
-    $phone = isset($_POST['phone']) ? $_POST['phone'] : "";
+    require_once '../../isOwner/isOwner.php';
+    require_once __DIR__ . '/../../controllers/AuthController.php';
 
-    $clientQuery = "INSERT INTO clients(name, address, numberPhone) VALUES(?,?,?)";
-    $params = array($name, $address, $phone);
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $address = $_POST['address'];
+        $number_phone = $_POST['number_phone'];
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm_password'];
+        $role = 0;
 
-    $resultClientQuery = $conn->prepare($clientQuery);
-    if($resultClientQuery->execute($params)) {
-        header('location:users.php?alert=success_add');
+        // insert new client
+        $auth = new AuthController($name, $email, $address, $number_phone, $password, $confirm_password, $role);
+        $createClient = $auth->register();
+        if($createClient) {
+            header('location:clients.php?alert=success_add');
+        }
     }
+
 ?>
