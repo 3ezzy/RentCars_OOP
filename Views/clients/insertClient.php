38dff9a49a4f5c09bf66f07e94c9322473755b48
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-27 20:03:02 +0100

    update name folder of all this files

---
+++ Views/clients/insertClient.php
@@ -0,0 +1,16 @@
+<?php
+     require_once('../../isOwner/isOwner.php');
+    require_once('../../connectdb/connectiondb.php');
+    // insert new client
+    $name = isset($_POST['name']) ? $_POST['name'] : "";
+    $address = isset($_POST['address']) ? $_POST['address'] : "";
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : "";
+
+    $clientQuery = "INSERT INTO clients(name, address, numberPhone) VALUES(?,?,?)";
+    $params = array($name, $address, $phone);
+
+    $resultClientQuery = $conn->prepare($clientQuery);
+    if($resultClientQuery->execute($params)) {
+        header('location:users.php?alert=success_add');
+    }
+?>
