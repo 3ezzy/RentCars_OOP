fc36ecbe3bd7123564f9579d3b418c9f762d4d5f
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-29 17:51:06 +0100

    refactor: fix editVoiture form and add image upload functionality

--- Views/voitures/editVoiture.php
+++ Views/voitures/editVoiture.php
@@ -30,13 +30,23 @@ if (isset($_GET['idEditVoiture'])) {
                 <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" name="marque" value="<?php echo $resultVoiture['marque'] ?>" id="marque2" placeholder="Enter marque car">
             </div>
         </div>
+        <div class="md:flex gap-3 justify-between mb-4">
+            <div class="flex flex-col w-full md:w-2/4 mb-4 md:mb-0">
+                <label class="ml-2" for="modele2">Modele <span class="text-red-600">*</span></label>
+                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" name="modele" value="<?php echo $resultVoiture['modele'] ?>" id="modele2" placeholder="Enter modele car">
+            </div>
+            <div class="flex flex-col w-full md:w-2/4 mb-4 md:mb-0">
+                <label class="ml-2" for="year">Year <span class="text-red-600">*</span></label>
+                <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="number" name="annee" value="<?php echo $resultVoiture['annee'] ?>" id="year" placeholder="Enter year car">
+            </div>
+        </div>
         <div class="flex flex-col mb-4">
-            <label class="ml-2" for="modele2">Modele <span class="text-red-600">*</span></label>
-            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" name="modele" value="<?php echo $resultVoiture['modele'] ?>" id="modele2" placeholder="Enter modele car">
+            <label class="ml-2" for="priceHour">Price / hour <span class="text-red-600">*</span></label>
+            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="number" name="priceHour" value="<?php echo $resultVoiture['priceHour'] ?>" id="priceHour" placeholder="Enter price car for hour">
         </div>
         <div class="flex flex-col">
-            <label class="ml-2" for="year">Year <span class="text-red-600">*</span></label>
-            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="text" name="annee" value="<?php echo $resultVoiture['annee'] ?>" id="year" placeholder="Enter year car">
+            <label class="ml-2" for="year">Upload Image <span class="text-red-600">*</span></label>
+            <input class="px-3 py-2 border-2 border-gray-400 rounded-md mt-1" type="file" name="imageCar" value="<?php echo $resultVoiture['image'] ?>" id="imageCar">
         </div>
         <div class="mt-5 flex justify-between">
             <button id="closeEdit" class="px-3 py-2 bg-red-600 text-white rounded-md hover:bg-red-400" type="button">Close</button>
