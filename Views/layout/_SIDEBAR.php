8feee51245103f646c55960cf245db3cb88013bb
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-12-29 15:51:35 +0100

    update sidebar to show pages of admin and pages of client

--- Views/layout/_SIDEBAR.php
+++ Views/layout/_SIDEBAR.php
@@ -1,6 +1,6 @@
 <?php $page = $_SERVER['PHP_SELF'];?>
 
-<div class="bg-[#2a2455] lg:rounded-md shadow-lg w-full flex items-center justify-between fixed left-0 bottom-0 md:static md:block md:h-screen md:flex-col md:w-1/5 lg:w-1/6 md:p-4 text-white">
+<div class="bg-[#2a2455] lg:rounded-md shadow-lg w-full flex items-center justify-between fixed left-0 bottom-0 md:bottom-3 md:static md:block md:h-screen md:flex-col md:w-1/5 lg:w-1/6 md:p-4 text-white">
     <div class="md:flex md:flex-col items-center justify-center text-xl md:mb-8 hidden">
         <?php echo "<img width='80' src='../../src/img/images_sidebar/logo.png' alt=''>" ?>
         <h1 class="font-semibold text-[#837bbe]">RENTAL CAR</h1>
@@ -14,36 +14,48 @@
         </div>
     </div>
     
-    <h1 class="hidden lg:block">Dashboard</h1>
-    <div class="">
-        <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center <?php if($page == '/views/dashboard.php') echo 'bg-[#595480]' ?>" href="/views/dashboard.php">
-            <?php echo "<img class='p-1 bg-blue-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/pie-chart.svg' alt=''>" ?>
+    <?php if($_SESSION['user']['role'] == true) { ?>
+        <h1 class="hidden lg:block">Dashboard</h1>
+        <div class="">
+            <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center <?php if($page == '/views/dashboard.php') echo 'bg-[#595480]' ?>" href="/views/dashboard.php">
+                <?php echo "<img class='p-1 bg-blue-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/pie-chart.svg' alt=''>" ?>
+                <span class="hidden lg:block ml-2">
+                    Dashboard
+                </span>
+            </a>
+
+        </div>
+
+        <h1 class="hidden lg:block md:mt-4 mb-1">Pages</h1>
+        <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center mb-1 <?php if($page == '/views/clients/clients.php') echo 'bg-[#595480]' ?>" href="/views/clients/clients.php">
+            <?php echo "<img class='p-1 bg-emerald-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/users.svg' alt=''>" ?>
             <span class="hidden lg:block ml-2">
-                Dashboard
+                Clients
+            </span>
+        </a>
+        
+        <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center mb-1 <?php if($page == '/views/voitures/voitures.php') echo 'bg-[#595480]' ?>" href="/views/voitures/voitures.php">
+            <?php echo "<img class='p-1 bg-cyan-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/car.svg' alt=''>" ?>
+            <span class="hidden lg:block ml-2">
+                Cars
             </span>
         </a>
 
-    </div>
-
-    <h1 class="hidden lg:block md:mt-4 mb-1">Pages</h1>
-    <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center mb-1 <?php if($page == '/views/clients/clients.php') echo 'bg-[#595480]' ?>" href="/views/clients/clients.php">
-        <?php echo "<img class='p-1 bg-emerald-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/users.svg' alt=''>" ?>
-        <span class="hidden lg:block ml-2">
-            Clients
-        </span>
-    </a>
-    <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center mb-1 <?php if($page == '/views/voitures/voitures.php') echo 'bg-[#595480]' ?>" href="/views/voitures/voitures.php">
-        <?php echo "<img class='p-1 bg-cyan-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/car.svg' alt=''>" ?>
-        <span class="hidden lg:block ml-2">
-            Cars
-        </span>
-    </a>
-    <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center <?php if($page == '/views/contrats/contrats.php') echo 'bg-[#595480]' ?>" href="/views/contrats/contrats.php">
-        <?php echo "<img class='p-1 bg-purple-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/contract.svg' alt=''>" ?>
-        <span class="hidden lg:block ml-2">
-            Contracts
-        </span>
-    </a>
+        <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center <?php if($page == '/views/contrats/contrats.php') echo 'bg-[#595480]' ?>" href="/views/contrats/contrats.php">
+            <?php echo "<img class='p-1 bg-purple-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/contract.svg' alt=''>" ?>
+            <span class="hidden lg:block ml-2">
+                Contracts
+            </span>
+        </a>
+    <?php } else { ?>
+        <h1 class="hidden lg:block md:mt-4 mb-1">Pages</h1>
+        <a class="p-2 hover:bg-[#595480] rounded-sm flex items-center mb-1 <?php if($page == '/views/pageClient/voitures.php') echo 'bg-[#595480]' ?>" href="/views/pageClient/voitures.php">
+            <?php echo "<img class='p-1 bg-cyan-600 rounded-md w-9 h-9' src='../../src/img/images_sidebar/car.svg' alt=''>" ?>
+            <span class="hidden lg:block ml-2">
+                Cars
+            </span>
+        </a>
+    <?php } ?>
 
     <h1 class="hidden lg:block mt-4">Log out</h1>
     <div>
